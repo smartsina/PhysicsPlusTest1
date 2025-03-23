f4dbae278f206c2f7cec541d06e08c2d9507351a
smartsina <96626126+smartsina@users.noreply.github.com>
2025-03-24 03:10:31 +0330

    Add root layout

---
+++ src/app/layout.tsx
@@ -0,0 +1,13 @@
+import './globals.css'
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="fa" dir="rtl">
+      <body>{children}</body>
+    </html>
+  )
+}
